79b516665d386b877136b621637d81503ec1ebaa
Sands-45 <sandsqa@hotmail.com>
2025-07-19 12:48:56 +0200

    Adding getWorldRates

--- src/utils.ts
+++ src/utils.ts
@@ -11,4 +11,22 @@ const setRate = (num: number, currency: string): string => {
   return `${currency} ${num} is the new rate`;
 };
 
-export { getRate, setRate };
+const getWorldRates = async (): Promise<any> => {
+  try {
+    const res = await fetch(
+      "https://query1.finance.yahoo.com/v8/finance/chart/GME?interval=1d&range=1y"
+    );
+
+    if (!res.ok) {
+      return [];
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error: unknown) {
+    const message =
+      error instanceof Error ? error.message : "We ran into an erro";
+  }
+};
+
+export { getRate, setRate ,getWorldRates};
