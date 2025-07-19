8de3a975fef98cd847b34c67e68fcff303f0224f
Sands-45 <sandsqa@hotmail.com>
2025-07-19 05:10:22 +0200

    Minor update'

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // Don't make any changes to this file
 export * as utils from "./utils";
 export { default as app } from "./app";
-export { default as config } from "./extension.toml";
+export { default as config } from "./extension.json";
