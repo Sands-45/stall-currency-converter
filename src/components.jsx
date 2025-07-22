4ced19b2dd4ae4c66c1276d683f9f2f586640ff4
Sands-45 <sandsqa@hotmail.com>
2025-07-22 21:18:06 +0200

    Minor update

--- src/components.jsx
+++ src/components.jsx
@@ -1,5 +1,7 @@
+import { getWorldRates } from "./utils";
+
 const TestButton = () => {
-  return <button className="w-40 h-10 bg-red-500">Test UI</button>;
+  return <button onClick={getWorldRates} className="w-40 h-10 bg-red-500 rounded-lg shrink-0">Test UI</button>;
 };
 
 export { TestButton };
